b6ba74e19032e9d9d735c9ff42f17338542bb46f
Adam Crow <adamcrow63@gmail.com>
2020-06-25 00:01:48 +1000

    Added function for pushing notes

--- src/main/java/life/genny/notes/models/Note.java
+++ src/main/java/life/genny/notes/models/Note.java
@@ -22,8 +22,6 @@ import javax.validation.constraints.NotNull;
 //import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 import org.jboss.logging.Logger;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.panache.common.Page;
@@ -45,7 +43,6 @@ public class Note extends PanacheEntity {
 
 	private static final Logger log = Logger.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
 	public static final String DEFAULT_TAG = "default";
-	protected final static String DATE_FORMAT_STR_ISO8601 = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
 
 	@JsonbTypeAdapter(LocalDateTimeAdapter.class)
 	public LocalDateTime created = LocalDateTime.now(ZoneId.of("UTC"));
