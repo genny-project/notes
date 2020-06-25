f91279d69e33d50b8faff90b70c8d8ac6051b5ec
Adam Crow <adamcrow63@gmail.com>
2020-06-25 11:27:01 +1000

    Added push script. Added cors

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+version=3.1.0
 #./mvnw clean package -Pnative -Dnative-image.docker-build=true
 pushd $PWD
 ./mvnw clean package -Dquarkus.container-image.build=true -DskipTests=true
-docker tag ${USER}/notes:3.1.0  gennyproject/notes:latest 
-#docker build -f src/main/docker/Dockerfile.jvm  -t gennyproject/notes:latest .
+docker tag ${USER}/notes:${version}  gennyproject/notes:latest 
+docker tag ${USER}/notes:${version}  gennyproject/notes:${version} 
 popd
