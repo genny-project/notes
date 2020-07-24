16563ac184a587363104863feb2f28ec659eda94
Adam Crow <adamcrow63@gmail.com>
2020-07-24 15:31:59 +1000

    Sped up java

--- build-docker-native.sh
+++ build-docker-native.sh
@@ -5,6 +5,7 @@
 #mvn package -Pnative -Dquarkus.native.container-build=true  -DskipTests=true
 
 ./build-native.sh
+#./mvnw package -Pnative -Dquarkus.native.container-build=true -DskipTests=true
 docker build -f src/main/docker/Dockerfile.native -t gennyproject/notes:latest .
 docker tag gennyproject/notes:latest gennyproject/notes:7.0.0
 
