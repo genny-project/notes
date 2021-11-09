348c72e08e0281eaba6f0f09bbd92a7247013dd1
Adam Crow <adamcrow63@gmail.com>
2021-11-09 11:07:59 +1100

    Updated build docker native

--- build-docker-native.sh
+++ build-docker-native.sh
@@ -4,13 +4,14 @@ realm=gennyproject
 echo "Building native"
 
 pwd=${PWD##*/}
+pwd=notes
 function prop() {
   grep "${1}=" ${file} | cut -d'=' -f2
 }
 
 echo "Building docker ${realm}/${pwd}:${version}"
 
-./mvnw  package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true
+./mvnw  package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true -DskipTests=true
 version=$(cat src/main/resources/${pwd}-git.properties | grep 'git.build.version' | cut -d'=' -f2)
 #version=$(grep 'git.build.version')
 echo $version
